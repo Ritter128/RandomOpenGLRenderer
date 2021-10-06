4fbc86d4646a63caea61ea57b58e6d7f5ef50920
Evan Weeks <evanweeks500@gmail.com>
2021-10-06 13:04:57 -0400

    Successfully created window!

--- main.c
+++ main.c
@@ -1,35 +1,37 @@
-#include <stdio.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
+    GLFWwindow* window;
+
+    /* Initialize the library */
     if (!glfwInit())
-    {
-        printf("BRUH\n");
-    }
+        return -1;
 
-    GLFWwindow* window = glfwCreateWindow(
-        600, 400, "LEARNCMAKE!", NULL, NULL
-    );
-
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
-        printf("Could not create window\n");
+        glfwTerminate();
+        return -1;
     }
 
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    if (!glfwWindowShouldClose(window))
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
     {
-
+        /* Render here */
         //glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
-
     return 0;
 }
