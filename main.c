c8238acfa2227b1b769535baf9ce1dce174073bb
Evan Weeks <evanweeks500@gmail.com>
2021-10-06 16:23:53 -0400

    Standard build commit

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
