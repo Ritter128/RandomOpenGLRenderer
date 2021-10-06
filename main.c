66af62e4d735982d9a13b7de89191b6cf24d3195
Evan Weeks <evanweeks500@gmail.com>
2021-10-06 16:21:39 -0400

    Standard build commit

--- main.c
+++ main.c
@@ -45,12 +45,16 @@ int main(void)
          0.5f, -0.5f
     };
 
+    /* Vertex buffer */
     unsigned int vertexBufferID;
     glGenBuffers(1, &vertexBufferID);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
+    /* Vertex attributes */
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+    glEnableVertexAtrribArray(0);
+        
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
