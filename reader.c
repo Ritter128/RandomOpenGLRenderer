f95dfa47c5e7e1f8a992afef6f807ab4ecada357
Evan Weeks <evanweeks500@gmail.com>
2021-10-06 20:30:21 -0400

    Standard build commit

--- reader.c
+++ reader.c
@@ -15,12 +15,19 @@ const char* ReadFile(const char* fileName)
 {
     FILE* pFile = fopen(fileName, "r");
     const char* outContents;
+    int fileSize;
 
     if (pFile == NULL)
     {
         printf("Could not load %s\n", fileName);
         return "Cringe";
     }
+
+    fseek(pFile, 0, SEEK_END);
+    fileSize = ftell(pFile);
+    rewind(pFile);
+
+    printf("File size: %d\n", fileSize);
 
     while (fgets(outContents, sizeof(outContents), pFile));
 
