c153e9d95adbb860e5e5deac17e12b995418941e
Evan Weeks <evanweeks500@gmail.com>
2021-10-06 20:18:07 -0400

    Standard build commit

--- reader.c
+++ reader.c
@@ -1,11 +1,19 @@
 #include "reader.h"
 #include <stdio.h>
 
+/*
+char buffer[100];
+FILE *fp = fopen("filename", "r");                 // do not use "rb"
+while (fgets(buffer, sizeof(buffer), fp)) {
+... do something
+}
+fclose(fp);
+*/
+
 char* ReadFile(const char* fileName)
 {
     FILE* pFile = fopen(fileName, "r");
     char fileContents[500];
-    char currentChar;
 
     if (pFile == NULL)
     {
@@ -13,14 +21,7 @@ char* ReadFile(const char* fileName)
         return "Cringe";
     }
 
-    int iterator = 0;
-    while ((currentChar = fgetc(pFile)) != EOF)
-    {
-        //printf("%s\n", fileContents);
-        //printf("%c\n", currentChar);
-        fileContents[iterator] = currentChar;
-        iterator++;   
-    }
+    while (fgets(fileContents, sizeof(fileContents), pFile));
 
     fclose(pFile);
 
