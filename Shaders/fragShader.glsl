455f3b668916c2c7bd9a9df99bfc1d6407f265c4
Evan Weeks <evanweeks500@gmail.com>
2021-10-08 14:22:58 -0400

    Added working textures

--- Shaders/fragShader.glsl
+++ Shaders/fragShader.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 
 in vec4 vertColor;
+in vec2 texCoords;
 
 out vec4 FragColor;
 
+uniform sampler2D uTexSample;
+
 void main()
 {
-    FragColor = vec4(normalize(vertColor));
+    FragColor = texture2D(uTexSample, texCoords);
 }
--- main.cpp
+++ main.cpp
@@ -113,29 +113,29 @@ int main(void)
     /* Rendering code */
 
     float vertices[] = {
-        -0.5f, -0.5f,  0.5f, //0
-        -0.5f,  0.5f,  0.5f, //1
-         0.5f, -0.5f,  0.5f, //2
-         0.5f,  0.5f,  0.5f, //3
-         0.5f, -0.5f, -0.5f, //4
-         0.5f,  0.5f, -0.5f, //5
-        -0.5f, -0.5f, -0.5f, //6
-        -0.5f,  0.5f, -0.5f, //7
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //0
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, //1
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f, //2
+         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //3
+        // 0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //4
+        // 0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //5
+        //-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //6
+        //-0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //7
     };
 
     unsigned int indices[] = {
         0, 1, 2,
         1, 3, 2,
-        3, 2, 4,      
-        4, 3, 5,
-        0, 1, 6,
-        6, 1, 7,
-        4, 5, 6,
-        6, 7, 5,
-        1, 3, 7,      
-        7, 3, 5,
-        0, 2, 6,
-        6, 2, 4
+        //3, 2, 4,      
+        //4, 3, 5,
+        //0, 1, 6,
+        //6, 1, 7,
+        //4, 5, 6,
+        //6, 7, 5,
+        //1, 3, 7,      
+        //7, 3, 5,
+        //0, 2, 6,
+        //6, 2, 4
     };
 
     glViewport(0, 0, 600, 400);
@@ -143,12 +143,28 @@ int main(void)
     /* Load image */
 
     int w, h, nrChannels;
-    unsigned char* imageFile = stbi_load("terrain.png", &w, &h, &nrChannels, 4);
+    unsigned char* imageFile = stbi_load("Textures/dirt.jpg", &w, &h, &nrChannels, 4);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     if(!imageFile)
         std::cout << "Cringe\n";
     else 
         std::cout << "KEKKKKK\n";
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, 
+    GL_RGBA, GL_UNSIGNED_BYTE, imageFile);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    stbi_image_free(imageFile);
 
     /* Vertex Array */
     unsigned int vertexArrayID;
@@ -168,8 +184,10 @@ int main(void)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     /* Vertex attributes */
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     /* Shaders */
     std::string vsSource = ReadFile("Shaders/vertShader.glsl");
@@ -205,25 +223,21 @@ int main(void)
         glm::mat4 modelTranslationMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.0f, cubePos.z));
         glm::mat4 modelRotationMatrix = glm::rotate(modelMatrix, glm::radians(cubeRotX), glm::vec3(0.0f, 1.0f, 0.0f));
         modelMatrix = modelTranslationMatrix * modelRotationMatrix;
-        ///glm::mat4 viewMatrix = glm::mat4(1.0f);
-        //glm::mat4 translationViewMatrix = glm::translate(viewMatrix, glm::vec3(0.0f, 0.0f, cubePos.z));
-        //viewMatrix = glm::rotate(
-            //viewMatrix, camRotX, glm::vec3(1.0f, 0.0f, 0.0f)) * glm::translate(viewMatrix, glm::vec3(0.0f, camRotX, 0.0f));
         glm::mat4 projMatrix = glm::mat4(1.0f);
         projMatrix = glm::perspective(glm::radians(90.0f), (float)(600/400), 0.1f, 100.0f);
 
         /* Uniforms */
         int locModelMatrix = glGetUniformLocation(shaderProgram, "uModelMatrix");
         glUniformMatrix4fv(locModelMatrix, 1, GL_FALSE, glm::value_ptr(modelMatrix));
-        //int locViewMatrix = glGetUniformLocation(shaderProgram, "uViewMatrix");
-        //glUniformMatrix4fv(locViewMatrix, 1, GL_FALSE, glm::value_ptr(viewMatrix));
         int locProjMatrix = glGetUniformLocation(shaderProgram, "uProjMatrix");
         glUniformMatrix4fv(locProjMatrix, 1, GL_FALSE, glm::value_ptr(projMatrix)); 
+        int locTexSample = glGetUniformLocation(shaderProgram, "uTexSample");
+        glUniform1i(locTexSample, 0);
         
         //std::cout << "CAMERA POSITION Z: " << cubePos.z << "\n";
         //std::cout << "CUBE ROT X: " << cubeRotX << "\n";
 
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
 
