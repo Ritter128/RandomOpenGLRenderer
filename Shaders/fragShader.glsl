1f7f71f2a22c2de69c5b3c109d6e0ea7b49188e3
Evan Weeks <evanweeks500@gmail.com>
2021-10-09 12:45:19 -0400

    Fake lighting added

--- Shaders/fragShader.glsl
+++ Shaders/fragShader.glsl
@@ -7,7 +7,15 @@ out vec4 FragColor;
 
 uniform sampler2D uTexSample;
 
+vec4 CalculateAverage(in vec4 vec)
+{
+    float total = vec.r + vec.g + vec.b + vec.a;
+    float avg = total/4;
+    vec4 newVec = vec4(avg);
+    return newVec;
+}
+
 void main()
 {
-    FragColor = texture2D(uTexSample, texCoords);
+    FragColor = texture2D(uTexSample, texCoords) * CalculateAverage(vertColor);
 }
